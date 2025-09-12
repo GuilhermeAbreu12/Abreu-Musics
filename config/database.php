c8a195a230ef20e764561a39a991771e6c1e9d54
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-09-12 09:40:51 -0300

    fix: fix the arrow function sign

--- config/database.php
+++ config/database.php
@@ -9,9 +9,9 @@ $charset = "utf8mb4";
 // Data Source Name (DSN)
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
-    PDO::ATTR_ERRMODE -> PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE -> PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES -> false,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
 try {
