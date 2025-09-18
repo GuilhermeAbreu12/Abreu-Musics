fecfc5b5f7f6b7716b10075ee5aa0baffa3754e8
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-09-18 00:01:57 -0300

    docs: add comments

--- src/create_song.php
+++ src/create_song.php
@@ -1,12 +1,13 @@
 <?php 
-require_once('../config/database.php');
+require_once('../config/database.php'); // Contecta com o banco
 
-header('Content-Type: application/json');
+header('Content-Type: application/json'); // transforma em JSON
 
-$data = json_decode(file_get_contents('php://input'), true);
+$data = json_decode(file_get_contents('php://input'), true); // Transforma a requisição Post+JSON em um array PHP chamado $data
 
+// Se não existir algum desses campos
 if (!isset($data['titulo'], $data['artista'], $data['caminho_arquivo'])){
-    http_response_code(400);
+    http_response_code(400); // Retorna um erro 400 em JSON
     echo json_encode(['mensagem' => 'Dados incompletos']);
     exit;
 }
@@ -14,17 +15,17 @@ if (!isset($data['titulo'], $data['artista'], $data['caminho_arquivo'])){
 try {
     $sql = "INSERT INTO songs (titulo, artista, album, caminho_arquivo) VALUES (?, ?, ?, ?)";
 
-    $stmt = $pdo -> prepare($sql);
+    $stmt = $pdo -> prepare($sql); // Prepare evita SQL injection
 
-    $stmt -> execute([
+    $stmt -> execute([ // Execute evita SQL injection
         $data['titulo'],
         $data['artista'],
-        $data['album'] ?? null,
+        $data['album'] ?? null, // Se não tiver album, coloca null
         $data['caminho_arquivo']
     ]);
 
     echo json_encode(['mensagem' => 'Música adicionada com sucesso!', 'id' => $pdo -> lastInsertId()]);
-} catch (\PDOException $e) {
+} catch (\PDOException $e) { // Se der erro no Banco, retorna mensagem de erro código 500
     http_response_code(500);
     echo json_encode(['mensagem' => 'Erro ao adicionar a música:'.$e->getMessage()]);
 }
