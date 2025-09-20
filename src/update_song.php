23ad5d3a1ba21827329b6a8a919b8e035487e137
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-09-20 00:26:32 -0300

    feat: add edit song function

--- src/update_song.php
+++ src/update_song.php
@@ -6,7 +6,7 @@ header('Content-Type: application/json');
 $id = $_GET['id'] ?? null;
 $data = json_decode(file_get_contents('php://input'), true);
 
-if (!id || !isset($data['titulo'], $data['artista'])) {
+if (!$id || !isset($data['titulo'], $data['artista'])) {
     http_response_code(400);
     echo json_encode(['mensagem' => 'Dados inválidos. ID, título e artista são obrigatórios.']);
     exit;
@@ -26,9 +26,11 @@ try {
     if ($stmt -> rowCount()){
         echo json_encode(['mensagem' => 'Música atualizada com sucesso!']);
     } else {
-        http_response_code(500);
-        echo json_encode(['mensagem' => 'Erro ao atualizar a música:'.$e->getMessage()]);
+        http_response_code(404);
+        echo json_encode(['mensagem' => 'Música não encontrada ou nenhum dado alterado.']);
     }
+} catch (\PDOException $e){
+    http_response_code(500);
+    echo json_encode(['mensagem' => 'Erro ao atualizar a música:'.$e->getMessage()]);
 }
-
 ?>
